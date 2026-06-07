fcef9b079aa1a47f5eaa5bfec20651e58e384788
sanzhar192 <zhampeissovsanzhar@gmail.com>
2026-06-07 18:55:45 +0500

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import telebot
-bot=telebot.TeleBot("")
+from config import token
+bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Привет, я бот!")
+@bot.message_handler(func=lambda message: message.text=="Привет")
+def send_image(message):
+    with open("image.jpg", "rb") as image:
+        bot.send_photo(message.chat.id, image)
 bot.polling()
